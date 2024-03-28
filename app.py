018cb9418c322a28d7861b0add7db950cddb80a1
Kiran <kiran@cerebrumx.ai>
2024-03-28 12:21:19 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('space.html')
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+@app.route("/video",methods=['GET','POST'])
+def video():
+    # requests.get(url='http://127.0.0.1:5000/static/script.js')
+    return render_template("video.html")
+if __name__ == '__main__':
+    app.run(debug=True)
